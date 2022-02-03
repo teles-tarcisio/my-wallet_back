323718dccbc2ba3ef56cf18271dd736558437744
Tarcísio Teles <tfst@ecomp.poli.br>
2022-02-03 16:35:14 -0300

    test/bugfix: fixed correct use of 'cors()', 'delayed response' simulation inside a route is working

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,12 +1,20 @@
 function simulateAxios(value) {
   return new Promise(resolve =>
-    setTimeout(() => resolve({...value, status: 'processed'}), 1500)
+    setTimeout(() => resolve({...value, status: 'processed'}), 2500)
   );
 }
 
 export async function createUser(req, res) {
-  const newUser = req.body;
-  const response = await simulateAxios(newUser);
-  return res.status(501).send(response);
-  
+  console.log('in: ', req.body);
+  const delay = simulateAxios(req.body);
+  delay.then((resolve) => {
+    console.log(resolve);
+    res.send(resolve);
+    return;
+  });
+}
+
+export function findUser(req, res) {
+  res.sendStatus(501);
+  return;
 }
--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import { createUser } from './controllers/usersController.js';
+import { createUser, findUser } from './controllers/usersController.js';
 
 const server = express();
 server.use(express.json());
-server.use(cors);
+server.use(cors());
 
 server.post('/users', createUser);
 
-
-
+server.get('/users', findUser);
 
 
 const serverPort = 5000;
